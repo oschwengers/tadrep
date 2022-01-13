734ea9d0216c73a02dca1d0d41002cb483c6b689
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-01-13 15:24:49 +0100

    update usage

--- tadrep/utils.py
+++ tadrep/utils.py
@@ -27,10 +27,9 @@ def parse_arguments():
     arg_group_io = parser.add_argument_group('Input / Output')
     arg_group_io.add_argument('--genome', '-g', action='store', default=None, nargs="+", help='Draft genome path')
     arg_group_io.add_argument('--plasmids', '-p', action='store', default=None, help='Plasmids path')
-    arg_group_io.add_argument('--output', '-o', action='store', default=os.getcwd(), help='Output directory (default = current working directory)')
-    arg_group_io.add_argument('--tmp-dir', action='store', default=None, help='Temporary directory to store blast hits')
-    arg_group_io.add_argument('--prefix', action='store', default=None, type=not_empty, help='Prefix for all output files (default = None)')
     arg_group_io.add_argument('--db', action='store', default=None, help='Directory which contains blast database')
+    arg_group_io.add_argument('--output', '-o', action='store', default=os.getcwd(), help='Output directory (default = current working directory)')
+    arg_group_io.add_argument('--prefix', action='store', default=None, type=not_empty, help='Prefix for all output files (default = None)')
     
     arg_group_parameters = parser.add_argument_group('Annotation')
     arg_group_parameters.add_argument('--min-contig-coverage', action='store', type=int, default=90, choices=range(1, 101), metavar='[1-100]', dest='min_contig_coverage', help="Minimal contig coverage (default = 90%%)")
@@ -43,6 +42,7 @@ def parse_arguments():
     arg_group_general.add_argument('--help', '-h', action='help', help='Show this help message and exit')
     arg_group_general.add_argument('--verbose', '-v', action='store_true', help='Print verbose information')
     arg_group_general.add_argument('--threads', '-t', action='store', type=is_positive, default=mp.cpu_count(), help='Number of threads to use (default = number of available CPUs)')
+    arg_group_general.add_argument('--tmp-dir', action='store', default=None, help='Temporary directory to store blast hits')
     arg_group_general.add_argument('--version', action='version', version='%(prog)s ' + tadrep.__version__)
     return parser.parse_args()
 
