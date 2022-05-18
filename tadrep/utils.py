6e5aa0edf6cb30529354140ec27415bd9770da46
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2022-05-18 15:51:15 +0200

    update db checks

--- tadrep/utils.py
+++ tadrep/utils.py
@@ -2,7 +2,6 @@ import argparse
 import logging
 import multiprocessing as mp
 import os
-import re
 import subprocess as sp
 import sys
 
@@ -13,8 +12,7 @@ import tadrep
 
 log = logging.getLogger('UTILS')
 
-FILES_V4 = ['db.nhr', 'db.nin', 'db.nsq', 'db.tsv']
-FILES_V5 = ['db.not', 'db.ntf', 'db.nto', 'db.ndb']
+DB_FILES = ['db.tsv', 'db.ndb', 'db.not', 'db.ntf', 'db.nto']
 
 CITATION = 'Schwengers et al. (2022)\nTaDReP: Targeted Detection and Reconstruction of Plasmids.\nGitHub https://github.com/oschwengers/tadrep'
 
@@ -106,8 +104,7 @@ def check_db_directory(db_path):
         if (not resolved_db_path.exists()):
             log.error('Database path does not exist! path=%s', resolved_db_path)
             sys.exit(f'ERROR: database path ({resolved_db_path}) does not exists!')
-        files = FILES_V4 + FILES_V5 if check_db_version(resolved_db_path) else FILES_V4
-        for file in files:
+        for file in DB_FILES:
             file_path = resolved_db_path.joinpath(file)
             if (not file_path.is_file()):
                 log.error('Database file missing! file=%s', file_path)
@@ -116,32 +113,3 @@ def check_db_directory(db_path):
         log.error('provided database not valid! path=%s', db_path)
         sys.exit(f'ERROR: provided database {db_path} not valid!')
     return resolved_db_path
-
-
-def check_db_version(db_path):
-    cmd_version = [
-        'blastdbcmd',
-        '-db', str(db_path.joinpath('db')),
-        '-info'
-    ]
-    log.info('Blastdb cmd: %s', cmd_version)
-    regex = r'(Version:\s)(\d+)'
-    try:
-        db_output = sp.check_output(cmd_version, stderr=sp.STDOUT, universal_newlines=True)
-        log.debug('Blastdb cmd output: %s', db_output)
-        result = re.search(regex, str(db_output))
-        log.info('Regex result: %s', result)
-    except FileNotFoundError:
-        log.error('Dependency not found! tool=%s', cmd_version[0])
-        sys.exit(f'ERROR: Command {cmd_version[0]} not found or not executable!')
-    except sp.CalledProcessError as e:
-        log.error('Dependency failed! tool=%s', cmd_version[0])
-        log.warning('Subprocess error: %s', e.stdout)
-        sys.exit(f'ERROR: {cmd_version[0]} could not be executed!\n{e.stdout}')
-
-    if(not result):
-        return False
-    log.info('Database version: %s', result.group(2))
-    if(int(result.group(2)) == 5):
-        return True
-    return False
