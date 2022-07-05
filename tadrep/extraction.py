00b4e41389c1c59693199a269ad73ab3143992bd
Adrian Guthmann <adrian.guthmann@hrz.uni-giessen.de>
2022-07-05 15:49:20 +0200

    identify and drop non circular genomes #2

--- tadrep/extraction.py
+++ tadrep/extraction.py
@@ -5,13 +5,19 @@ import tadrep.io as tio
 def extract():
     plasmids = []
     for file in cfg.plasmids_to_extract:
-        circular_genomes = tio.import_sequences(file)
-        for id, genome in circular_genomes.items():
+        genomes = tio.import_sequences(file)
+        not_circular_ids = []
+        for id, genome in genomes.items():
             if('circular=true' not in genome['description']):
-                circular_genomes.pop(id)
+                not_circular_ids.append(id)
                 continue
             genome['file'] = file.name
-            
+        
+        circular_genomes = {k: genomes[k] for k in genomes.keys() if k not in not_circular_ids}
+
+        if(not circular_genomes):
+            continue
+
         longest_genome = sorted(circular_genomes.values(), key=lambda k: k['length'])[-1]
 
         if(longest_genome['length'] > 112_000):
