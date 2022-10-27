848ee1834d86ee8e4f13205d91764673ac7a146d
Adrian Guthmann <adrian.tim.guthmann@bioinfsys.uni-giessen.de>
2022-10-27 11:50:03 +0200

    create method to check for existing json files #2

--- tadrep/io.py
+++ tadrep/io.py
@@ -84,3 +84,13 @@ def export_json(data, json_path):
     log.info('write json: path=%s, # sequences=%i', json_path, len(data))
     with open(json_path, 'w') as fh:
         json.dump(data, fh)
+
+
+def check_existing_JSON(json_path):
+    if json_path.is_file():
+        log.info('%s existing', json_path)
+        plasmid_dict = import_json(json_path)
+    else:
+        log.info('%s NOT existing', json_path)
+        plasmid_dict = {}
+    return plasmid_dict
