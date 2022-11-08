ed82e8089078e89bb1c1d013f69a515a4a69e31a
Adrian Guthmann <adrian.tim.guthmann@bioinfsys.uni-giessen.de>
2022-11-08 09:47:19 +0100

    add method todo description #2

--- tadrep/extraction.py
+++ tadrep/extraction.py
@@ -7,36 +7,37 @@ log = logging.getLogger('EXTRACT')
 
 
 def extract():
-    # Main methode
-    # Call methods matching input files
+    # get existing json existing_plasmid_dict
+    # update counter    plasmid_count
+
+    # load files
+        # call genome/draft/plasmid methods
+        # update existing_plasmid_dict
+    # export to json
     pass
 
 
-def draft_extract():
-    # extract sequences from draft files
-    pass
+def draft_extract(seq_dict):
+    # remove all sequences without 'circular' 'complete' 'plasmid' or custom header
+    return seq_dict
 
 
-def genome_extract():
-    # extract sequences from whole genome files
-    pass
+def genome_extract(seq_dict):
+    # read all sequences from input excluding longest
+    # find longest X sequences and remove them
+    return seq_dict
 
 
-def plasmids_extract():
-    # extract sequences from plasmids files
-    pass
+def plasmids_extract(seq_dict):
+    # read all sequences from input file without filtering
+    return seq_dict
 
 
-def search_headers():
+def search_headers(seq_dict):
     # search headers for 'plasmid' 'complete' or custom string
-    pass
+    return seq_dict
 
 
-def filter_longest():
+def filter_longest(seq_dict):
     # filter out longest sequences
-    pass
-
-
-def check_existing_JSON():
-    # load JSON file if existing
-    pass
+    return seq_dict
