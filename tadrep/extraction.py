2a4324702969850114ebf68ba60c13e0e62a56a8
Adrian Guthmann <adrian.tim.guthmann@bioinfsys.uni-giessen.de>
2022-11-10 15:25:23 +0100

    combine for loops #2

--- tadrep/extraction.py
+++ tadrep/extraction.py
@@ -12,18 +12,33 @@ def extract():
     json_output_path = cfg.output_path.joinpath('extraction.json')
     existing_plasmid_dict = tio.load_data(json_output_path)
     
-    # update counter plasmid_count
+    # update plasmid count
     number_of_plasmids = max([id for id in existing_plasmid_dict.keys()])
     verboseprint(f'found {number_of_plasmids} existing plasmids!')
     log.info('found %d existing plasmids in file %s', number_of_plasmids, json_output_path)
 
-    # call genome/draft/plasmid methods
-    if(cfg.file_type == 'genome'):
-        new_plasmids = genome_extract(number_of_plasmids)
-    elif(cfg.file_type == 'plasmids'):
-        new_plasmids = plasmid_extract(number_of_plasmids)
-    else:
-        new_plasmids = draft_extract(number_of_plasmids)
+    new_plasmids = {}
+
+    # load sequences
+    for input_file in cfg.files_to_extract:
+
+        file_sequences = tio.import_sequences(input_file, sequences=True)
+        verboseprint(f'file: {input_file.name}, sequences: {len(file_sequences)}')
+        log.info('file: %s, sequences: %d', input_file.name, len(file_sequences))
+
+        # call genome/draft/plasmid methods
+        if(cfg.file_type == 'genome'):
+            file_sequences = filter_longest(file_sequences)
+        elif(cfg.file_type == 'draft'):
+            file_sequences = search_headers(file_sequences)
+        
+        # add file name and new id to plasmids
+        for sequence in file_sequences:
+            sequence['file'] = input_file.name
+            sequence['new_id'] = plasmid_count
+
+            new_plasmids[plasmid_count] = sequence
+            plasmid_count += 1
 
     # update existing_plasmid_dict
     existing_plasmid_dict.update(new_plasmids)
@@ -32,73 +47,6 @@ def extract():
     
     # export to json
     tio.export_json(existing_plasmid_dict, json_output_path)
-
-
-def draft_extract(plasmid_count):
-    # remove all sequences without 'circular' 'complete' 'plasmid' or custom header
-    new_plasmids = {}
-
-    for input_file in cfg.files_to_extract:
-
-        file_sequences = tio.import_sequences(input_file, sequence=True)
-        verboseprint(f'file: {input_file.name}, sequences: {len(file_sequences)}')
-        log.info('file: %s, sequences: %d', input_file.name, len(file_sequences))
-
-        file_sequences = search_headers(file_sequences)
-
-        for sequence in file_sequences:
-            sequence['file'] = input_file.name
-            sequence['new_id'] = plasmid_count
-
-            new_plasmids[plasmid_count] = sequence
-            plasmid_count += 1
-
-    return seq_dict
-
-
-def genome_extract(plasmid_count):
-    # read all sequences from input excluding longest
-    new_plasmids = {}
-
-    for input_file in cfg.files_to_extract:
-
-        file_sequences = tio.import_sequences(input_file, sequence=True)
-        verboseprint(f'file: {input_file.name}, sequences: {len(file_sequences)}')
-        log.info('file: %s, sequences: %d', input_file.name, len(file_sequences))
-
-        file_sequences = filter_longest(file_sequences)
-
-        for sequence in file_sequences:
-            sequence['file'] = input_file.name
-            sequence['new_id'] = plasmid_count
-
-            new_plasmids[plasmid_count] = sequence
-            plasmid_count += 1
-
-    return new_plasmids
-
-
-def plasmid_extract(plasmid_count):
-    # read all sequences from input file without filtering
-    new_plasmids = {}
-
-    # for each file
-    for input_file in cfg.files_to_extract:
-        # read file     tio.import_sequences sequences=true
-        file_plasmids = tio.import_sequences(input_file, sequences=True)
-        verboseprint(f'file: {input_file.name}, sequences: {len(file_plasmids)}')
-        log.info('file: %s, sequences: %d', input_file.name, len(file_plasmids))
-        
-        for plasmid in file_plasmids:
-            # add filename to each sequence in dict
-            plasmid['file'] = input_file.name
-            plasmid['new_id'] = plasmid_count
-
-            # add each plasmid to new dict with plasmid_count as key
-            new_plasmids[plasmid_count] = plasmid
-            plasmid_count += 1
-        
-    return new_plasmids
 
 
 def search_headers(seq_dict):
