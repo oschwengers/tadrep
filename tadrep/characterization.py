9b00535771ad680eca1c66903613f018e50baa75
Adrian Guthmann <adrian.tim.guthmann@bioinfsys.uni-giessen.de>
2022-11-17 12:20:37 +0100

    fill method with functionality #5

--- tadrep/characterization.py
+++ tadrep/characterization.py
@@ -1,23 +1,45 @@
 import logging
+import sys
+
+import tadrep.io as tio
+import tadrep.config as cfg
 
 log = logging.getLogger('CHARACTERIZE')
 
+
 def characterize():
     # load json from output path
+    json_path = cfg.output_path.joinpath('extraction.json')
+    existing_data = tio.load_data(json_path)
+    loaded_plasmids = existing_data.get('extraction', {})
+
     # check if data is available
-    # check if seq is available
-    # check if length is available / update length if not
-    # calculate GC content
-    # calculate INC_types (Platon)
-    pass
+    if(not existing_data or not loaded_plasmids):
+        log.error('ERROR: Failed to load data from %s', json_path)
+        sys.exit(f'ERROR: Failed to load data from {json_path}!')
+
+    for plasmid in loaded_plasmids:
+        # set length
+        plasmid['length'] = len(plasmid['sequence'])
+
+        # set gc_content
+        plasmid['gc_content'] = gc_content(plasmid['sequence'])
+        # calculate INC_types (Platon)
+
+    # update json
+    tio.export_json(existing_data, json_path)
 
 
-def calc_length():
-    pass
+def gc_content(sequence):
+    # count C and G occurenxes
+    c_content = sequence.count('C')
+    g_content = sequence.count('G')
+    gc_combined = c_content + g_content
 
+    # calculate gc percentage
+    percentage = gc_combined / float(len(sequence))
 
-def gc_content():
-    pass
+    return percentage
 
 
 def inc_types():
