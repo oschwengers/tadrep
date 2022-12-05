6c5d7fc376292f9413afeb3265ba1e6588b96861
Adrian Guthmann <adrian.tim.guthmann@bioinfsys.uni-giessen.de>
2022-12-05 09:08:10 +0100

    temporarily removed inc type download #5

--- tadrep/characterization.py
+++ tadrep/characterization.py
@@ -20,7 +20,7 @@ def characterize():
         log.error('Failed to load data from %s', json_path)
         sys.exit(f'ERROR: Failed to load data from {json_path}!')
 
-    for plasmid in plasmids:
+    for plasmid in plasmids.values():
         # set length
         plasmid['length'] = len(plasmid['sequence'])
 
@@ -52,22 +52,13 @@ def gc_content(sequence):
 
 
 def download_inc_types():
-    
+
     inc_types_path = cfg.output_path.joinpath('inc-types.fasta')
-    
+
     # check if inc-types.fasta is already available
     if(inc_types_path.is_file()):
         return
-    
-    inc_types_cmd = [
-        'git clone https://bitbucket.org/genomicepidemiology/plasmidfinder_db.git;',
-        'for f in plasmidfinder_db/*.fsa; do cat $f >> inc-types-raw.fasta; done;',
-        'cat inc-types-raw.fasta | sed -r "s/^>([a-zA-Z0-9()]+)_([0-9]+)_(.*)_(.*)$/>\1 \4/" > inc-types-mixed.fasta;',
-        "awk '{if(!/>/){print toupper($0)}else{print $1}}' inc-types-mixed.fasta > ", f"{inc_types_path};",
-        'rm -rf plasmidfinder_db inc-types-raw.fasta inc-types-mixed.fasta'
-    ]
 
-    tu.run_cmd(inc_types_cmd, cfg.output_path)
     return
 
 
