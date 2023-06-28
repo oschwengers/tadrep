13bc0c0cfe6258a4d991c027a58570bddc50b370
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2023-06-28 15:47:30 +0200

    implement first clustering

--- tadrep/cluster.py
+++ tadrep/cluster.py
@@ -11,55 +11,52 @@ def cluster_plasmids():
     db_path = cfg.output_path.joinpath('db.json')
     db_data = tio.load_data(db_path)
 
-    cluster = []
-    # define method / criteria for clustering
 
-    if(cfg.skip_cluster):
-        # build clusters
-        for plasmid in db_data['plasmids'].values():
-            new_cluster = {
-                'representative': plasmid['id'],
-                'members': [plasmid['id']]
-            }
-            cluster.append(new_cluster)
+    # write multifasta
+    fasta_path = cfg.tmp_path.joinpath('plasmids.extracted.fna')
+    tio.export_sequences(db_data['plasmids'].values(), fasta_path)
 
-        db_data['cluster'] = cluster
-        prepare_blastdb(db_data)
+    # cluster sequences
+    cmd_cdhitest = [
+        'cd-hit-est',
+        '-i', str(fasta_path),
+        '-o', str(cfg.tmp_path.joinpath('plasmids.clustered')),
+        '-c', '0.8',
+        '-S', '1000',
+        '-g', '1'
+    ]
+    log.debug('cmd=%s', cmd_cdhitest)
+    tu.run_cmd(cmd_cdhitest, cfg.tmp_path)
 
-    # find cluster representative
+    # parse clusters
+    clusters = []
+    current_cluster = None
+    with cfg.tmp_path.joinpath('plasmids.clustered.clstr').open('r') as fh:
+        for line in fh:
+            line = line.strip()
+            if line.startswith('>'):
+                if current_cluster is not None:
+                    clusters.append(current_cluster)
+                current_cluster = {
+                    'id': line[1:].split(' ')[1],  # cluster number
+                    'representative': None,
+                    'members': []
+                }
+            else:
+                plasmid_id = line.split('\t')[1].split(' ')[1][1:-3]  # exmaple: "0	6222nt, >And12566.short-33... *"
+                current_cluster['members'].append(plasmid_id)
+                if '*' in line:
+                    current_cluster['representative'] = plasmid_id
+        if current_cluster is not None:
+            clusters.append(current_cluster)
 
+    db_data['cluster'] = clusters
+
+    cfg.verbose_print('Detected plasmid clusters:')
+    for cluster in clusters:
+        members = ', '.join(cluster['members'])
+        cfg.verbose_print(f"Cluster {cluster['id']}\n\tsize: {len(cluster['members'])}\n\trepresentative: {cluster['representative']}\n\tmembers: {members}")
+        log.info('cluster: %s, size: %d, rep: %s, members: %s', cluster['id'], len(cluster['members']), cluster['representative'], cluster['members'])
+    
     # write json
     tio.export_json(db_data, db_path)
-
-
-def prepare_blastdb(db_data):
-
-    representative_ids = []
-
-    # Get representatives from JSON file
-    for cluster in db_data['cluster']:
-        rep_id = cluster['representative']
-        representative_ids.append(rep_id)
-
-    reference_plasmids = {id: db_data['plasmids'][id] for id in representative_ids}
-
-    cfg.verbose_print(f"Found {len(representative_ids)} representative plasmid(s)")
-    log.info("Found %d representative plasmid(s)", len(representative_ids))
-
-    # write multifasta for blast search
-    fasta_tmp_path = cfg.tmp_path.joinpath("db.fasta")
-    tio.export_sequences(reference_plasmids.values(), fasta_tmp_path)
-
-    db_output_path = cfg.output_path.joinpath('reference_db/db')
-    create_blast_db(db_output_path, fasta_tmp_path, cfg.tmp_path)
-
-
-def create_blast_db(output_path, fasta_tmp_path, tmp_path):
-    cmd_blastdb = [
-        'makeblastdb',
-        '-in', str(fasta_tmp_path),
-        '-dbtype', 'nucl',
-        '-out', str(output_path)
-    ]
-    log.debug('cmd=%s', cmd_blastdb)
-    tu.run_cmd(cmd_blastdb, tmp_path)
