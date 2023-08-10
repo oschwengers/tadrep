519f6fb95a9c6040e08cb16bf97e3bdbc4a0f21b
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2023-08-10 13:08:32 +0200

    remove deprecated PyTest marks

--- test/test_tadrep.py
+++ test/test_tadrep.py
@@ -3,7 +3,6 @@ import pytest
 from subprocess import run
 
 
-@pytest.mark.slow
 def test_full(tmpdir):
     # all parameter OK
     proc = run(['bin/tadrep', '--genome', 'test/data/draft.fna', '--plasmids', 'test/data/plasmids.fna', '--output', tmpdir])
