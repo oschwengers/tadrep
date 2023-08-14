8fdb3ef8a0f362d0dc8a7204b0cf247aa60e8d32
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2023-08-14 11:42:33 +0200

    fix blackbox tadrep CI test

--- test/test_tadrep.py
+++ test/test_tadrep.py
@@ -1,3 +1,5 @@
+import shutil as sh
+
 from pathlib import Path
 from subprocess import run
 
@@ -6,13 +8,14 @@ import pytest
 from .conftest import FILES
 
 def test_full(tmpdir):
-    # all parameter OK
-    proc = run(['bin/tadrep', 'detect', '--genome', 'test/data/draft.fna', '--plasmids', 'test/data/plasmids.fna', '--output', tmpdir])
-    
-    assert proc.returncode == 0
-
     tmpdir_path = Path(tmpdir)
-    for file in FILES:
+    sh.copyfile(Path('test/data/db.json'), tmpdir_path.joinpath('db.json'))
+    
+    # all parameter OK
+    proc = run(['bin/tadrep', '--output', tmpdir, 'detect', '--genome', 'test/data/draft.fna'])
+    assert proc.returncode == 0
+    
+    for file in FILES:  # all files in place and not empty
         output_path = tmpdir_path.joinpath(file)
         assert Path.exists(output_path)
         assert output_path.stat().st_size > 0
